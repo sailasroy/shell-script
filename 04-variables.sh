e97bde04a6a30331456b98c2d2b3d1bdfc149dba
sailasroy <sailasroy123@gmail.com>
2023-08-16 16:51:14 +0530

    names inside command added

---
+++ 04-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+
+PERSON1=$1
+PERSON2=$2
+
+
+echo "$PERSON1: hi $PERSON2"
+echo "$PERSON2:hi $PERSON1"
+echo "$PERSON1:how are you $PERSON2"
+echo "$PERSON2:i am fine $PERSON1 how are you"
