b25bf0e512c51c36521370ca17e0bd5dd743c794
sailasroy <sailasroy123@gmail.com>
2023-08-16 16:35:30 +0530

    hello world added

--- 01-hello1.sh
+++ 01-hello1.sh
@@ -5,5 +5,5 @@ echo "Hello world"
 
 echo "roy: hi dilli"
 echo "dilli:hi roy"
-echo"roy:how are you dilli"
-echo"dilli:i am fine roy how are you"
+echo "roy:how are you dilli"
+echo "dilli:i am fine roy how are you"
