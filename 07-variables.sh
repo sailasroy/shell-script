b43ae1f8a27e4ca8e4d860eb56ef3b9099a6b223
sailasroy <sailasroy123@gmail.com>
2023-08-16 17:06:11 +0530

    CREDENTIALS added but hidden

---
+++ 07-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash echo "Enter username"
+
+read -s USERNAME
+
+echo " usename entered is : $USERNAME"
+
+echo "Enter password"
+
+read -s PASSWORD
+
+echo "password entered is : $PASSWORD"
