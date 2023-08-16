d6e60fba707b4d7e132c9980ef073a5ef388ea72
sailasroy <sailasroy123@gmail.com>
2023-08-16 16:40:46 +0530

    hello world added

---
+++ 02-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$roy
+PERSON2=$dilli
+
+echo "$PERSON1: hi $PERSON2"
+echo "$PERSON2:hi $PERSON1"
+echo "$PERSON1:how are you $PERSON2"
+echo "$PERSON2:i am fine $PERSON1 how are you"
