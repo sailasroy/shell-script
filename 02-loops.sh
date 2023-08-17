02450f89d1243b5a78c6f8d32f48571b49dc7444
sailasroy <sailasroy123@gmail.com>
2023-08-17 16:27:42 +0530

    loops added

--- 02-loops.sh
+++ 02-loops.sh
@@ -4,10 +4,10 @@ USERID=$(id -u)
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo -e "$i ....$R FAILURE $N"
+        echo -e "Installing $i ....$R FAILURE $N"
          exit 1
     else
-         echo -e "$i ....$G SUCCESS $N"
+         echo -e " Installing $i ....$G SUCCESS $N"
     fi
 
 }
@@ -29,5 +29,5 @@ for i in $@
 do 
  
 yum install $i -y >>$LOGFILE
-VALIDATE $? "Installing $i "
+VALIDATE $? "$i"
 done
