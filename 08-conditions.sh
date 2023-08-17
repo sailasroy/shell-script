cf30e2b996bf2dd54d6e9e56a8c12753cb84089a
sailasroy <sailasroy123@gmail.com>
2023-08-17 16:06:11 +0530

    loops added

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -23,7 +23,7 @@ if [ $USERID -ne 0 ]
     exit 1
     fi 
 
-    yum install mysql -y &>>$LOGFILE
+    yum install mysqqql -y &>>$LOGFILE
     VALIDATE $? "Installing Mysql"
     # if [ $? -ne 0 ]
     # then
