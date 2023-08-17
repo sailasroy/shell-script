4519b9914dcf15285e74bc97f09481b5b773bb9e
sailasroy <sailasroy123@gmail.com>
2023-08-17 15:40:18 +0530

    loops added

--- 02-loops.sh
+++ 02-loops.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
 
+USERID=$(id -u)
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ....$R FAILURE $N"
+         exit 1
+    else
+         echo -e "$2 ....$G SUCCESS $N"
+    fi
+
+}
+    DATE=$(date +%F)
+    SCRIPT_NAME=$0
+    LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
+    R="\e[31m"
+    G="\e[32m"
+    N="\e[0m"
+if [ $USERID -ne 0 ]
+    then
+    echo "ERROR :: sign in with root access"
+    exit 1
+    fi 
+
+
 for i in $@
 do 
 yum install $i -y
