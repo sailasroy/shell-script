5bb816779d5ffa052d8110280f6c58e7b0af6024
sailasroy <sailasroy123@gmail.com>
2023-08-17 15:25:22 +0530

    Function added

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -4,17 +4,19 @@ USERID=$(id -u)
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo "$2 .... FAILURE"
+        echo -e "$2 ....$R FAILURE $N"
          exit 1
     else
-         echo "$2 .... SUCCESS"
+         echo "$2 ....$G SUCCESS $N"
     fi
 
 }
     DATE=$(date +%F)
     SCRIPT_NAME=$0
     LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
-
+    R=\e[31m
+    G=\e[32m
+    N=\e[0m
 if [ $USERID -ne 0 ]
     then
     echo "ERROR :: sign in with root access"
