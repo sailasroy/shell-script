6f5e67bb480c9045ab0fb1d72aef788a3ce640a8
sailasroy <sailasroy123@gmail.com>
2023-08-18 14:07:53 +0530

    new assignment

--- as2-delete-oldlogs.sh
+++ as2-delete-oldlogs.sh
@@ -6,10 +6,23 @@ LOGDIR=/home/centos/script-logs
 SCRIPT_NAME=$0
 LOGFILE=$LOGDIR/$SCRIPT_NAME-$DATE.log
 
+# finding the specified files/logs at the given time or date
 
 FILES_TO_DELETE=$(find $APPS_DIR_FOLDER -name "*.log" -type f -mtime +14)
 
-echo "$FILES_TO_DELETE"
+echo "$FILES_TO_DELETE" # prints the old logs 
+
+## TO Delete the log files we will use while loop
+
+while read line
+
+    do 
+    # write statements here
+        echo " Deleting $line" &>>$LOGFILE
+        rm -rf $line
+
+    done <<< $FILES_TO_DELETE
+
 
 
 
