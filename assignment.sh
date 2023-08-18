4ec2398871e3be5a0163dd807a511e6854dd7fbc
sailasroy <sailasroy123@gmail.com>
2023-08-18 12:56:27 +0530

    new assignment

--- assignment.sh
+++ assignment.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
 USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
-    echo "ERROR :: sign in with root access "
+    echo -e "$R ERROR :: sign in with root access $N "
     exit 1
 fi
