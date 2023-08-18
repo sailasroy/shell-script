79c2398186c3077d9e746e6f2965ef63f8b0e276
sailasroy <sailasroy123@gmail.com>
2023-08-18 13:58:53 +0530

    new assignment

--- as2-delete-oldlogs.sh
+++ as2-delete-oldlogs.sh
@@ -7,7 +7,7 @@ SCRIPT_NAME=$0
 LOGFILE=$LOGDIR/$SCRIPT_NAME-$DATE.log
 
 
-FILES_TO_DELETE=$(find $APPS_DIR_FOLDER -name "*.log" -type f -mname +14)
+FILES_TO_DELETE=$(find $APPS_DIR_FOLDER -name "*.log" -type f -mtime +14)
 
 echo "$FILES_TO_DELETE"
 
