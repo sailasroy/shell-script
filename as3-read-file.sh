1c9b8387cffe28a546510499855488fac3991dfa
sailasroy <sailasroy123@gmail.com>
2023-08-19 15:46:25 +0530

    as3 assignment

--- as3-read-file.sh
+++ as3-read-file.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
     FILENAME=/home/centos/roy/dillis.txt
     #n=1
-    FILE_FREQUENCY=$(cat $FILENAME | tr -s ' ' '\n' | awk '{nums[$1]++}END{for(word in nums) print word, nums[word]}' | sort -rn -k2)
+    ##FILE_FREQUENCY=$(cat $FILENAME | tr -s ' ' '\n' | awk '{nums[$1]++}END{for(word in nums) print word, nums[word]}' | sort -rn -k2)
     number_of_lines=`wc --words < $FILENAME`
     REPLACE_WORD=$(sed -i 's/roy/santu/g' $FILENAME)
     while read line
     do 
     ##echo "Line no. $n : $line"
 
-    echo "$FILE_FREQUENCY"
+    echo "$FILENAME"
     #echo "$character"
     ##n=$((n+1))
 
