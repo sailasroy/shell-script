ea967d03fcf13e77516440fb1d85438b0114e6b2
sailasroy <sailasroy123@gmail.com>
2023-08-19 13:54:18 +0530

    assignment 3

--- as3-read-file.sh
+++ as3-read-file.sh
@@ -2,7 +2,7 @@
     FILENAME=/home/centos/roy/dillis.txt
     #n=1
     FILE_FREQUENCY=$(cat $FILENAME | tr -s ' ' '\n' | awk '{nums[$1]++}END{for(word in nums) print word, nums[word]}' | sort -rn -k2)
-    
+    number_of_lines=`wc --lines < $FILENAME`
     while read line
     do 
     ##echo "Line no. $n : $line"
@@ -12,3 +12,5 @@
     ##n=$((n+1))
 
     done <$FILE_FREQUENCY 
+
+        echo "number of lines =$number_of_lines"
