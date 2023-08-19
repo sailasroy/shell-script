286f01b8cd730c3599e2ef243fc1e71e9a16f41a
sailasroy <sailasroy123@gmail.com>
2023-08-19 16:37:40 +0530

    as4 palindrome

--- as4-palindrome.sh
+++ as4-palindrome.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
-echo "Enter a String"
-read input
-reverse=""
-
-len=${#input}
-for (( i=$len-1; i>=0; i-- ))
-do 
-	reverse="$reverse${input:$i:1}"
-done
-if [ $input == $reverse ]
+ echo Enter the string
+read s
+echo $s>temp
+rvs="$(rev temp)"
+if [ $s = $rvs ]
 then
-    echo "$input is palindrome"
+echo "it is palindrome"
 else
-    echo "$input is not palindrome"
+echo " it is not a Palindrome"
 fi
