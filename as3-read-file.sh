d76963058d70b44fe863d3bb43e99324400481bc
sailasroy <sailasroy123@gmail.com>
2023-08-19 15:37:38 +0530

    as3 assignment

--- as3-read-file.sh
+++ as3-read-file.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
     FILENAME=/home/centos/roy/dillis.txt
     #n=1
-    FILE_FREQUENCY=$(cat $FILENAME | awk '{for(i=1;i<=NF;++i) { arr[$i]++; } } END { x=0; for(var in arr) {newarr[arr[var]]=var; if(arr[var]>x) x=arr[var];} for(i=x;i>0;--i) if (newarr[i] > 0)')
+    FILE_FREQUENCY=$(cat $FILENAME | tr -s ' ' '\n' | awk '{nums[$1]++}END{for(word in nums) print word, nums[word]}' | sort -rn -k2)
     number_of_lines=`wc --words < $FILENAME`
     while read line
     do 
