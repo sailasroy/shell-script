1234af09a676f9e6a8f446e6df982386fd449b09
sailasroy <sailasroy123@gmail.com>
2023-08-19 15:44:38 +0530

    as3 assignment

--- as3-read-file.sh
+++ as3-read-file.sh
@@ -3,6 +3,7 @@
     #n=1
     FILE_FREQUENCY=$(cat $FILENAME | tr -s ' ' '\n' | awk '{nums[$1]++}END{for(word in nums) print word, nums[word]}' | sort -rn -k2)
     number_of_lines=`wc --words < $FILENAME`
+    REPLACE_WORD=$(sed -i 's/roy/santu/g' $FILENAME)
     while read line
     do 
     ##echo "Line no. $n : $line"
@@ -11,6 +12,6 @@
     #echo "$character"
     ##n=$((n+1))
 
-    done <$FILE_FREQUENCY 
+    done <$FILENAME
 
         echo "number of words =$number_of_lines"
