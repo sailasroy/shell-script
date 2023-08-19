77bf719231058016a510d0149c9e7d239b6a737c
sailasroy <sailasroy123@gmail.com>
2023-08-19 12:57:45 +0530

    assignment 3

--- as3-read-file.sh
+++ as3-read-file.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
     FILENAME=/home/centos/roy/dilli.txt
 
-    while IFS=' ' read in line
+    while IFS=' ' read in line -n1 character
     uniq -c $line
     do 
 
     echo "$line"
+    echo "$character"
 
     done < $FILENAME
