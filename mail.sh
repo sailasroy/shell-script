240fe313a65f5594fc75beebd29658c9482fea6d
sailasroy <sailasroy123@gmail.com>
2023-08-21 18:59:58 +0530

    as6 disk usage

--- mail.sh
+++ mail.sh
@@ -2,10 +2,10 @@
 
 TO_ADDRESS=$!
 SUBJECT=$2
- BODY=$(sed -e 's/[]\/$*.^[]/\\&/g' <<< $3)
+BODY=$(sed -e 's/[]\/$*.^[]/\\&/g' <<< $3)
 ALERT_TYPE=$4
 TEAM_NAME=$5
 
-FINAL_BODY=$(sed -e "s/TEAM_NAME/$TEAMNAME/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -r "s/MESSAGE/$BODY/g" template.html)
+FINAL_BODY=$(sed -e "s/TEAM_NAME/$TEAM_NAME/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -r "s/MESSAGE/$BODY/g" template.html)
 ##echo ("all args : $@")
 echo "$FINAL_BODY" | mail -s "$(echo -e "$SUBJECT \n Content-Type: text/html")" "$TO_ADDRESS"
