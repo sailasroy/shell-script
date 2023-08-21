e702fe2d6d60333877a97fe915327ffbff57c6d9
sailasroy <sailasroy123@gmail.com>
2023-08-21 15:34:45 +0530

    as5 disk usage

---
+++ as5-disk-usage.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+R="\e[31m" 
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+LOG_DIR=/tmp
+DATE=$(date +%F:%H:%M:%S)
+SCRIPT_NAME=$0
+LOGFILE=$LOGDIR/$SCRIPT_NAME-$DATE.log
+DISK_USAGE=$(grep -vE 'tmpfs|Filesystem)'
+DISK_USAGE_THRESHOLD=1
+
+while IFS= read line
+do
+echp "output : $line"
+
+done <<< $DISK_USAGE
+
