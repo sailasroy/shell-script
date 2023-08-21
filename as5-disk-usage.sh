58baf20aad78d9f1ea8c3cd452c3cb7bbdd1de8d
sailasroy <sailasroy123@gmail.com>
2023-08-21 15:45:03 +0530

    as4 palindrome

--- as5-disk-usage.sh
+++ as5-disk-usage.sh
@@ -8,12 +8,13 @@ LOG_DIR=/tmp
 DATE=$(date +%F:%H:%M:%S)
 SCRIPT_NAME=$0
 LOGFILE=$LOGDIR/$SCRIPT_NAME-$DATE.log
-DISK_USAGE=$(grep -vE 'tmpfs|Filesystem')
+DISK_USAGE=$(df -hT | grep -vE 'tmpfs|Filesysytem')
 DISK_USAGE_THRESHOLD=1
 
 while IFS= read line
 do
-echp "output : $line"
+##echp "output : $line"
+usage=$(echo $line | awk '{print $6F}' )
 
 done <<< $DISK_USAGE
 
