1cf3bdd30af4fc4d25a28f4b9a414bed9358005c
sailasroy <sailasroy123@gmail.com>
2023-08-21 16:54:09 +0530

    as6 disk usage

--- as5-disk-usage.sh
+++ as5-disk-usage.sh
@@ -1,4 +1,35 @@
-#!/bin/bash
+# #!/bin/bash
+# R="\e[31m" 
+# G="\e[32m"
+# N="\e[0m"
+# Y="\e[33m"
+
+# LOG_DIR=/tmp
+# DATE=$(date +%F:%H:%M:%S)
+# SCRIPT_NAME=$0
+# LOGFILE=$LOGDIR/$SCRIPT_NAME-$DATE.log
+# DISK_USAGE=$(df -hT | grep -vE 'tmpfs|Filesysytem')
+# DISK_USAGE_THRESHOLD=1
+# message=""
+
+# while IFS= read line
+# do
+# ##echp "output : $line"
+# ##usage=$(echo $line | awk '{print $6}' | cut -d % -f1)
+# usage=$(echo $line | awk '{print $6}' | cut -d % -f1)
+
+# partition=$(echo $line| awk '{print $1}')
+# ##echo "$usage"
+# ####if [ $usage -gt $DISK_USAGE_THRESHOLD ];
+# if [ $usage -gt $DISK_USAGE_THRESHOLD ];
+# then 
+#   message+="High Disk Usage on $partition : $usage \n"
+# fi
+
+# done <<< $DISK_USAGE
+# echo -e "message: $message "
+
+#!bin/bash
 R="\e[31m" 
 G="\e[32m"
 N="\e[0m"
@@ -8,26 +39,21 @@ LOG_DIR=/tmp
 DATE=$(date +%F:%H:%M:%S)
 SCRIPT_NAME=$0
 LOGFILE=$LOGDIR/$SCRIPT_NAME-$DATE.log
-DISK_USAGE=$(df -hT | grep -vE 'tmpfs|Filesysytem')
+
+DISK_USAGE=$(df -hT | grep -vE 'tmpfs|Filesystem')
 DISK_USAGE_THRESHOLD=1
 message=""
-
 while IFS= read line
-do
-##echp "output : $line"
-##usage=$(echo $line | awk '{print $6}' | cut -d % -f1)
-usage=$(echo $line | awk '{print $6}' | cut -d % -f1)
+do 
+ usage=$(echo $line | awk '{print $6}' | cut -d % -f1)
+ partition=$(echo $line | awk '{print $1}')
 
-partition=$(echo $line| awk '{print $1}')
-##echo "$usage"
-####if [ $usage -gt $DISK_USAGE_THRESHOLD ];
-if [ $usage -gt $DISK_USAGE_THRESHOLD ];
-then 
-  message+="High Disk Usage on $partition : $usage \n"
-fi
-
+ if [ $usage -gt $DISK_USAGE_THRESHOLD ];
+ then 
+ message+="HIGH DISK USAGE ON $partition : $usage \n"
 done <<< $DISK_USAGE
-echo -e "message: $message "
+
+echo -e "message: $message"
 
 
 
