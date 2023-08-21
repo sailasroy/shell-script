a4fc0da32ae25fec89418f3ce15a35942c7a18d7
sailasroy <sailasroy123@gmail.com>
2023-08-21 16:34:51 +0530

    as5 disk usage

--- as5-disk-usage.sh
+++ as5-disk-usage.sh
@@ -15,7 +15,9 @@ message=""
 while IFS= read line
 do
 ##echp "output : $line"
+##usage=$(echo $line | awk '{print $6}' | cut -d % -f1)
 usage=$(echo $line | awk '{print $6}' | cut -d % -f1)
+
 partition=$(echo $line| awk '{print $1}')
 ##echo "$usage"
 ####if [ $usage -gt $DISK_USAGE_THRESHOLD ];
