52a496a04d17412dc7f72be628b07c99872b1696
sailasroy <sailasroy123@gmail.com>
2023-08-24 15:29:54 +0530

    Adding ec2

--- create-ec2.sh
+++ create-ec2.sh
@@ -20,7 +20,7 @@ do
        IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
         echo "created instance $i : $IP_ADDRESS"
 
-        $ aws route53 change-resource-record-sets --hosted-zone-id Z02662462B6M0IFT9U3J6 --change-batch '
+        aws route53 change-resource-record-sets --hosted-zone-id Z02662462B6M0IFT9U3J6 --change-batch '
          {
             "Comment": "CREATE/DELETE/UPSERT a record ",
             "Changes": [{
