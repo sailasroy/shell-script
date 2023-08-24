92fcc4d2d3801a9ba73b557e86d58f519a16ac51
sailasroy <sailasroy123@gmail.com>
2023-08-24 13:49:21 +0530

    Adding ec2

--- create-ec2.sh
+++ create-ec2.sh
@@ -5,9 +5,9 @@ INSTANCE_TYPE=""
 IMAGE_ID=ami-03265a0778a880afb
 SECURITY_GROUP_ID=sg-021d144635db8f0b8
 
-for i in "$NAMES [@]"
+for i in "$NAMES[@]"
 do
-  if [[ $i == 'mongodb' || $i == 'mysql' ]]
+  if [[ $i == "mongodb" || $i == "mysql" ]]
   then
     INSTANCE_TYPE='t3.medium'
   else
