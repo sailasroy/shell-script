b9dd01ce74caa843cbee2586c299b54de051fa3c
sailasroy <sailasroy123@gmail.com>
2023-08-24 13:50:12 +0530

    Adding ec2

--- create-ec2.sh
+++ create-ec2.sh
@@ -15,7 +15,7 @@ do
   fi
   echo "creating $i instance"
 
-        aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instance[0].PrivateIpAddress'
+        aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value="$i"}]" | jq -r '.Instance[0].PrivateIpAddress'
 
        ### IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
 
