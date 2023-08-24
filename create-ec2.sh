d1f179e45a387bb6a6c1b60807f09571f54d467a
sailasroy <sailasroy123@gmail.com>
2023-08-24 15:27:22 +0530

    Adding ec2

--- create-ec2.sh
+++ create-ec2.sh
@@ -4,6 +4,7 @@ NAMES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "cart" "user" "shipping"
 INSTANCE_TYPE=""
 IMAGE_ID=ami-03265a0778a880afb
 SECURITY_GROUP_ID=sg-021d144635db8f0b8
+DOMAIN_NAME=sailasdevops.online
 
 for i in "${NAMES[@]}"
 do
@@ -18,6 +19,32 @@ do
 
        IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
         echo "created instance $i : $IP_ADDRESS"
+
+        $ aws route53 change-resource-record-sets --hosted-zone-id Z02662462B6M0IFT9U3J6 --change-batch '
+         {
+            "Comment": "CREATE/DELETE/UPSERT a record ",
+            "Changes": [{
+            "Action": "CREATE",
+                        "ResourceRecordSet": {
+                                    "Name": "'$i.$DOMAIN_NAME'",
+                                    "Type": "A",
+                                    "TTL": 300,
+                                 "ResourceRecords": [{ "Value": "'$IP_ADDRESS'"}]
+}}]
+}
+'
+####    #     aws route53 change-resource-record-sets --hosted-zone-id Z098285010FMU6PDV8O9P --change-batch '
+    # {
+    #         "Changes": [{
+    #         "Action": "CREATE",
+    #                     "ResourceRecordSet": {
+    #                         "Name": "'$i.$DOMAIN_NAME'",
+    #                         "Type": "A",
+    #                         "TTL": 300,
+    #                         "ResourceRecords": [{ "Value": "'$IP_ADDRESS'"}]
+    #                     }}]
+    # }
+    # '
 
         ##"ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instance[0].PrivateIpAddress'
 
