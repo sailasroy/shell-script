fa0f8749464d2cd68db05e3beb0b752a3bf732da
sailasroy <sailasroy123@gmail.com>
2023-08-24 13:43:46 +0530

    Adding ec2

--- create-ec2.sh
+++ create-ec2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-NAMES=$("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "cart" "user" "shipping" "payment" "dispatch" "web")
+NAMES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "cart" "user" "shipping" "payment" "dispatch" "web")
 INSTANCE_TYPE=""
 IMAGE_ID=ami-03265a0778a880afb
 SECURITY_GROUP_ID=sg-021d144635db8f0b8
@@ -8,6 +8,7 @@ SECURITY_GROUP_ID=sg-021d144635db8f0b8
 for i in $(NAMES [@])
 do
   if [[ $i == 'mongodb' || $i == 'mysql' ]]
+  then
     INSTANCE_TYPE='t3.medium'
   else
     INSTANCE_TYPE='t2.micro'
