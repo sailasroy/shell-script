9e38d6e19ed915d49ebe7295de16d34797c32e7a
sailasroy <sailasroy123@gmail.com>
2023-08-24 13:39:55 +0530

    Adding ec2

---
+++ create-ec2.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+NAMES=$("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "cart" "user" "shipping" "payment" "dispatch" "web")
+INSTANCE_TYPE=""
+IMAGE_ID=ami-03265a0778a880afb
+SECURITY_GROUP_ID=sg-021d144635db8f0b8
+
+for i in $(NAMES [@])
+do
+  if [[ $i == 'mongodb' || $i == 'mysql' ]]
+    INSTANCE_TYPE='t3.medium'
+  else
+    INSTANCE_TYPE='t2.micro'
+  fi
+  echo "creating $i instance"
+
+        aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instance[0].PrivateIpAddress'
+
+       ### IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
+
+
+done
