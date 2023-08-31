242638b3a0350dac0a1a710a5341c36e77319f1c
sailasroy <sailasroy123@gmail.com>
2023-08-31 11:12:19 +0530

    ec2 users addition

--- create-ec2.sh
+++ create-ec2.sh
@@ -5,7 +5,7 @@ INSTANCE_TYPE=""
 IMAGE_ID=ami-03265a0778a880afb
 SECURITY_GROUP_ID=sg-021d144635db8f0b8
 DOMAIN_NAME=sailasdevops.online
-
+HOSTED_ZONE_ID=Z0308214GYCUYHGJHT8R
 for i in "${NAMES[@]}"
 do
   if [[ $i == "mongodb" || $i == "mysql" ]]
@@ -20,7 +20,7 @@ do
        IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
         echo "created instance $i : $IP_ADDRESS"
 
-        aws route53 change-resource-record-sets --hosted-zone-id Z02662462B6M0IFT9U3J6 --change-batch '
+        aws route53 change-resource-record-sets --hosted-zone-id $HOSTED_ZONE_ID  --change-batch '
          {
             "Comment": "CREATE/DELETE/UPSERT a record ",
             "Changes": [{
